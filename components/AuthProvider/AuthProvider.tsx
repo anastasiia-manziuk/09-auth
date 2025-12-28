5107bccaa1d3782b81c63259e6da19bc4a443e73
ANASTASIIA <nastamanzuk62@gmail.com>
2025-12-28 17:13:04 +0100

    fix authprovider

--- components/AuthProvider/AuthProvider.tsx
+++ components/AuthProvider/AuthProvider.tsx
@@ -1,61 +1,44 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { usePathname, useRouter } from 'next/navigation';
-import { checkSession, getMe, logout } from '@/lib/api/clientApi';
+import { checkSession, getMe } from '@/lib/api/clientApi';
 import { useAuthStore } from '@/lib/store/authStore';
-
-const privateRoutes = ['/profile', '/notes'];
 
 export default function AuthProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const router = useRouter();
+  const [isChecking, setIsChecking] = useState(true);
 
-  const setUser = useAuthStore((state) => state.setUser);
+  const setUser = useAuthStore(state => state.setUser);
   const clearIsAuthenticated = useAuthStore(
-    (state) => state.clearIsAuthenticated
+    state => state.clearIsAuthenticated
   );
 
-  const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
-    const checkAuth = async () => {
-      setIsLoading(true);
+    const initAuth = async () => {
+      try {
+        const isAuthenticated = await checkSession();
 
-      const isPrivateRoute = privateRoutes.some((route) =>
-        pathname.startsWith(route)
-      );
-
-      const isAuthenticated = await checkSession();
-
-      if (isAuthenticated) {
-        const user = await getMe();
-        if (user) {
+        if (isAuthenticated) {
+          const user = await getMe();
           setUser(user);
-          setIsLoading(false);
-          return;
+        } else {
+          clearIsAuthenticated();
         }
+      } finally {
+        setIsChecking(false);
       }
-
-      if (isPrivateRoute) {
-        await logout();
-        clearIsAuthenticated();
-        router.push('/sign-in');
-      }
-
-      setIsLoading(false);
     };
 
-    checkAuth();
-  }, [pathname, setUser, clearIsAuthenticated, router]);
+    initAuth();
+  }, [setUser, clearIsAuthenticated]);
 
-  if (isLoading) {
+  if (isChecking) {
     return <p>Loading...</p>;
   }
 
-  return <>{children}</>;
+  return children;
 }
+
