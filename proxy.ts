5a293ca3ca44ba329620fa94b6c85acfb1b16ab6
ANASTASIIA <nastamanzuk62@gmail.com>
2025-12-29 20:59:22 +0100

    fix proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
+import { parse } from 'cookie';
 import { checkSession } from '@/lib/api/serverApi';
 
 const authRoutes = ['/sign-in', '/sign-up'];
@@ -7,23 +8,43 @@ const privateRoutes = ['/profile', '/notes'];
 
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
+  const cookieStore = await cookies();
 
-  const isAuthRoute = authRoutes.some(route =>
-    pathname.startsWith(route)
-  );
+  const accessToken = cookieStore.get('accessToken');
+  const refreshToken = cookieStore.get('refreshToken');
 
   const isPrivateRoute = privateRoutes.some(route =>
     pathname.startsWith(route)
   );
 
-  const cookieStore = await cookies();
-  const accessToken = cookieStore.get('accessToken');
-  const refreshToken = cookieStore.get('refreshToken');
-
+  const isAuthRoute = authRoutes.some(route =>
+    pathname.startsWith(route)
+  );
 
   if (!accessToken && refreshToken) {
     try {
-      await checkSession();
+      const response = await checkSession();
+
+      const setCookie = response.headers?.['set-cookie'];
+
+      if (setCookie) {
+        const cookieArray = Array.isArray(setCookie)
+          ? setCookie
+          : [setCookie];
+
+
+        for (const cookie of cookieArray) {
+          const parsed = parse(cookie);
+
+          if (parsed.accessToken) {
+            cookieStore.set('accessToken', parsed.accessToken);
+          }
+
+          if (parsed.refreshToken) {
+            cookieStore.set('refreshToken', parsed.refreshToken);
+          }
+        }
+      }
     } catch {
       if (isPrivateRoute) {
         return NextResponse.redirect(new URL('/sign-in', request.url));
@@ -31,11 +52,11 @@ export async function proxy(request: NextRequest) {
     }
   }
 
-  if (isPrivateRoute && !accessToken) {
+  if (isPrivateRoute && !cookieStore.get('accessToken')) {
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
 
-  if (isAuthRoute && accessToken) {
+  if (isAuthRoute && cookieStore.get('accessToken')) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
