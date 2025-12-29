1bf50006a82a7565515e548e604986afd1aeff52
ANASTASIIA <nastamanzuk62@gmail.com>
2025-12-29 18:46:59 +0100

    add file proxy

---
+++ middleware.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { checkSession } from '@/lib/api/serverApi';
+
+const authRoutes = ['/sign-in', '/sign-up'];
+const privateRoutes = ['/profile', '/notes'];
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const isAuthRoute = authRoutes.some(route =>
+    pathname.startsWith(route)
+  );
+
+  const isPrivateRoute = privateRoutes.some(route =>
+    pathname.startsWith(route)
+  );
+
+  const accessToken = request.cookies.get('accessToken');
+  const refreshToken = request.cookies.get('refreshToken');
+
+  let isAuthenticated = Boolean(accessToken);
+
+  // 🔹 Поновлення сесії через refreshToken
+  if (!accessToken && refreshToken) {
+    try {
+      await checkSession();
+      isAuthenticated = true;
+    } catch {
+      isAuthenticated = false;
+    }
+  }
+
+  if (isPrivateRoute && !isAuthenticated) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+ 
+  if (isAuthRoute && isAuthenticated) {
+    return NextResponse.redirect(new URL('/', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/profile/:path*', '/notes/:path*', '/sign-in', '/sign-up'],
+};
